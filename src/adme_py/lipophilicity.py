1984606cb17abc4788029b2b67d1cefce5c12df3
mcnaughtonadm <mcnaughtonadm@gmail.com>
2024-06-18 23:42:14 -0700

    Starting logp calculators, implementing Crippen Method and test.

---
+++ src/adme_py/lipophilicity.py
@@ -0,0 +1,19 @@
+"""Calculate Lipophilicity parameters."""
+
+from rdkit import Chem
+
+
+def calculate_logp_crippen(mol):
+    """Calculate the LogP of a molecule using Crippen Method.
+
+    Parameters
+    ----------
+         mol : Chem.Mol
+              The input rdkit Mol object.
+
+    Returns
+    -------
+         result : float
+              The calculated WLogP of the molecule.
+    """
+    return Chem.Crippen.MolLogP(mol)
