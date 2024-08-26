0951f1b8e0fdc913e16a8f1fd37d4e4d569fa741
mcnaughtonadm <mcnaughtonadm@gmail.com>
2024-08-25 22:39:09 -0700

    Small linting, adding logp class to ADME

--- src/adme_py/lipophilicity.py
+++ src/adme_py/lipophilicity.py
@@ -3,6 +3,23 @@
 from rdkit import Chem
 
 
+def calculate_all_lipophilicity(mol: Chem.Mol) -> dict[str, float]:
+    """Calculate the all the lipophilicity properties of a given molecule.
+
+    Parameters
+    ----------
+        mol : Chem.Mol
+             The input rdkit Mol object
+    """
+    wlogp: float = calculate_logp_crippen(mol)
+
+    properties: dict[str, float] = {
+        "wlogp": wlogp,
+    }
+
+    return properties
+
+
 def calculate_logp_crippen(mol):
     """Calculate the LogP of a molecule using Wildman-Crippen Method.
 
